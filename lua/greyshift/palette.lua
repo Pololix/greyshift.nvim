afa767ca73ac59017856a2bc31c9d162a2412215
Pololix <devpololix@gmail.com>
2026-05-01 17:15:21 +0200

    Added temporal colors

---
+++ lua/greyshift/palette.lua
@@ -0,0 +1,18 @@
+local M = {}
+M.dark = {
+    bg0     = "#1e1e2e",
+    bg1     = "#181825",
+    bg2     = "#313244",
+    fg0     = "#cdd6f4",
+    fg1     = "#bac2de",
+    red     = "#f38ba8",
+    green   = "#a6e3a1",
+    yellow  = "#f9e2af",
+    blue    = "#89b4fa",
+    magenta = "#cba6f7",
+    cyan    = "#89dceb",
+    white   = "#a6adc8",
+    none    = "NONE",
+}
+
+return M
