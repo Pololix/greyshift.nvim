5e4db2139ff58200016bf0e8e4a9e92f21d33b3b
Pololix <devpololix@gmail.com>
2026-05-01 20:30:53 +0200

    Fixed reamains of prior palette at init.lua

--- lua/greyshift/init.lua
+++ lua/greyshift/init.lua
@@ -11,14 +11,7 @@ function M.setup(opts)
 end
 
 function M.load()
-    local palette = require("greyshift.palette")
-    local highlights = require("greyshift.highlights")
-
-    if M.options.transparent_background then
-        palette.bg0 = "NONE"
-    end
-
-    highlights.setup(palette)
+    require("greyshift.highlights").setup(require("greyshift.palette"))
 end
 
 return M
